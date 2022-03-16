46442f937a51a1799cf953e14eeb0c195c3ab539
Simon-CHOU <mrsimonzhou@gmail.com>
2022-03-16 22:55:44 +0800

    init baeldung powermock intro

---
+++ baeldung-powermock-intro/src/main/java/com/simon/baeldungpowermockintro/BaeldungPowermockIntroApplication.java
@@ -0,0 +1,13 @@
+package com.simon.baeldungpowermockintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeldungPowermockIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaeldungPowermockIntroApplication.class, args);
+	}
+
+}
---
+++ baeldung-powermock-intro/src/test/java/com/simon/baeldungpowermockintro/BaeldungPowermockIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simon.baeldungpowermockintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeldungPowermockIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
